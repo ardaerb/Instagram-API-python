e9a62d17b316df59d07a80acb1bccbf47a410b18
ardaerb <43710152+ardaerb@users.noreply.github.com>
2018-10-25 22:50:06 +0300

    Unnecessary line at test.py
    Since the native response to a valid login is 'Login Success!' anyway, there's no need to include 2 of the same message.

--- examples/test.py
+++ examples/test.py
@@ -9,6 +9,5 @@ api = InstagramAPI("login", "password")
 if (api.login()):
     api.getSelfUserFeed()  # get self user feed
     print(api.LastJson)  # print last response JSON
-    print("Login succes!")
 else:
     print("Can't login!")
